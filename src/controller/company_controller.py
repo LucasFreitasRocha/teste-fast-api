5a8ce2aab6d6ab0f8ffe816d4d1c17dcbbc4a76f
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2025-07-17 21:25:28 -0300

    optmizing

--- src/controller/company_controller.py
+++ src/controller/company_controller.py
@@ -1,26 +1,14 @@
-from typing import Optional
-from fastapi import APIRouter
 import inject
-from pydantic import BaseModel
+from fastapi import APIRouter, status
 from src.service.company_service import CompanyService
 from src.service.domain.company.company_domain import CompanyDomain
 from src.service.domain.company.company_request_domain import CompanyRequest
 
 
-
-
-
-class CompanyResponse(BaseModel):
-    id: int
-    name: str
-    description: str
-    document: str
-
-
 router = APIRouter()
 
 
-@router.post("")
+@router.post("", response_model=CompanyDomain, status_code=status.HTTP_201_CREATED, response_model_exclude_none=True)
 def create_company(company: CompanyRequest):
     company_service = inject.instance(CompanyService)
     return company_service.create_company(
@@ -31,4 +19,3 @@ def create_company(company: CompanyRequest):
             apps=[],
         )
     )
-    
